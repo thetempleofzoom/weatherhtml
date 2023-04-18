8ed0c5f912aa93085b88d2d777781ca08a0e41e9
schen24 <schen638@yahoo.com>
2023-04-18 22:22:59 +0800

    created reference table for stations up to 100 only

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template
 import pandas as pd, numpy as np
+import pycountry
 
 # render template objects to all be kept in specially named 'templates' folder,
 # with this EXACT spelling
 
 app = Flask("__name__")
 
+table = pd.read_csv('data/stations.txt', skiprows=17)
+#only show up to station 100
+table.sort_values('STAID', inplace=True)
+last = np.where(table.STAID == 100)[0][0]
+table['Station Code'] = table['STAID'].astype(str)
+table['Location'] = table['STANAME                                 '].str.strip()
+#create list from country codes, then create dict to map to country name
+code = table['CN'].unique().tolist()
+ctrydict={}
+for c in code:
+    ctrydict[c] = pycountry.countries.get(alpha_2=c).name
+table['Country'] = table['CN'].map(ctrydict)
+cleantable = table[['Location', 'Country', 'Station Code']][:last+1]
 
 @app.route("/")  # slash for default page
 def home():
-    return render_template("weather.html")
+    cleantable = cleantable.to_html(index=False)
+    return render_template("weather.html", htmltable = cleantable)
 
 
 @app.route("/api/v1/<station>/<date>")  #note syntax for dynamic variables
