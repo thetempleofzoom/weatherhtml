87a35019fd09281d5f7c261359af4f71755de57a
schen24 <schen638@yahoo.com>
2023-04-18 23:21:18 +0800

    included yearly data and single station data

--- main.py
+++ main.py
@@ -19,22 +19,38 @@ ctrydict={}
 for c in code:
     ctrydict[c] = pycountry.countries.get(alpha_2=c).name
 table['Country'] = table['CN'].map(ctrydict)
-cleantable = table[['Location', 'Country', 'Station Code']][:last+1]
+
 
 @app.route("/")  # slash for default page
 def home():
+    cleantable = table[['Location', 'Country', 'Station Code']][:last + 1]
     cleantable = cleantable.to_html(index=False)
     return render_template("weather.html", htmltable = cleantable)
 
 
 @app.route("/api/v1/<station>/<date>")  #note syntax for dynamic variables
 def lookup(station, date):
-    date = int(date)
     # can use zfill (zerofill) for strings
-    df = pd.read_csv(f'data/TG_STAID000{int(station):03}.txt', skiprows=20)
+    df = pd.read_csv(f'data/TG_STAID000{int(station):03}.txt', skiprows=20, parse_dates=['    DATE'])
     df['TG'] = df['   TG'].mask(df['   TG'] == -9999, np.nan) / 10
     temperature = str(df.loc[df['    DATE'] == date]['TG'].squeeze())
     return {'station': station, 'date': date, 'temperature': temperature}
+
+@app.route("/api/v1/<station>")  #note syntax for dynamic variables
+def onestation(station):
+    # can use zfill (zerofill) for strings
+    df = pd.read_csv(f'data/TG_STAID{station.zfill(6)}.txt', skiprows=20, parse_dates=['    DATE'])
+    df['TG'] = df['   TG'].mask(df['   TG'] == -9999, np.nan) / 10
+    result = df.to_dict(orient='records')
+    return result
+
+@app.route("/api/v1/year/<station>/<year>")  #note syntax for dynamic variables
+def oneyear(station, year):
+    df = pd.read_csv(f'data/TG_STAID{station.zfill(6)}.txt', skiprows=20, parse_dates=['    DATE'])
+    df['TG'] = df['   TG'].mask(df['   TG'] == -9999, np.nan) / 10
+    df = df[df['    DATE'].dt.year == int(year)]
+    result = df.to_dict(orient='records')
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True) #can add eg port=5001 if running multiple apps.
